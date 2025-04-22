311138adaad920e252911df408ce249e0dae52a5
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-04-21 22:10:54 -0400

    [CLASE 9] Validacion

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -8,12 +8,22 @@ use App\Models\Repository;
 class RepositoryController extends Controller
 {
     public function store(Request $request) {
+        $request->validate([
+            'url' => 'required',
+            'description' => 'required',
+        ]);
+
         $request->user()->repositories()->create($request->all());
 
         return redirect()->route('repositories.index');
     }
     
     public function update(Request $request, Repository $repository) {
+        $request->validate([
+            'url' => 'required',
+            'description' => 'required',
+        ]);
+
         $repository->update($request->all());
 
         return redirect()->route('repositories.edit', $repository);
