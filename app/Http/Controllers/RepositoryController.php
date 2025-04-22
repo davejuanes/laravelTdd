c564dffee511f2e3f68632e926ac8d7b78e73fc1
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-04-21 21:47:43 -0400

    [CLASE 8] ACTUALIZAR REGISTRO

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Repository;
 
 class RepositoryController extends Controller
 {
@@ -10,5 +11,11 @@ class RepositoryController extends Controller
         $request->user()->repositories()->create($request->all());
 
         return redirect()->route('repositories.index');
+    }
+    
+    public function update(Request $request, Repository $repository) {
+        $repository->update($request->all());
+
+        return redirect()->route('repositories.edit', $repository);
     }
 }
